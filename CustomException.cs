22f5b3538d267c77a937629bc9fce00eab5fdb21
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-10-26 12:15:09 +0100

    Add files via upload

---
+++ CustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch1
+{
+    class CustomException : Exception
+    {
+        int Retries { get;  }
+        public string ErrorMessage { get; private set; }
+
+        public CustomException(int numberOfRetries)
+        {
+            Retries = numberOfRetries;
+            ErrorMessage = $"Max number of retries exceeded. Max allowed is {Retries}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Skriv en siffra mellan 1-10!");
+
+            int myNumber;
+
+            try
+            {
+                myNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException henric)
+            {
+                Console.WriteLine($"Ooops, something went wrong! Error message: {henric.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Gick något fel?");
+                Console.ReadLine();
+                //Detta block exekveras ALLTID. Bra ställe att tex stänga Filereaders/writers eller
+                //eller andra resurser som tex databaskopplingar
+            }
+
+            try
+            {
+                TryAgain(13);
+            }
+            catch (CustomException ce)
+            {
+                Console.WriteLine(ce.ErrorMessage);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// Min metod för att försöka igen
+        /// </summary>
+        /// <param name="numberOfRetries">Antalet försök</param>
+        public static void TryAgain(int numberOfRetries)
+        {
+            if (numberOfRetries > 12)
+            {
+                throw new CustomException(12);
+            }
+        }
+    }
+}
